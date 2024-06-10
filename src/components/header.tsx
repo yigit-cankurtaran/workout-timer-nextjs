b95543e33f3a7c4f74e4f275653ac1a6ee818446
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-10 16:07:43 +0300

    added a home link to header for easier testing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 function Header() {
-  return <header>i might handle the internal links here idk tho</header>;
+  return (
+    <header>
+      i might handle the internal links here idk tho <Link href="/">Home</Link>
+    </header>
+  );
   //   i might also straight up delete this file
 }
 
