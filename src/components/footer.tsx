fd67a93df13a54abc650ebfe1f6372dd8536ed4a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-20 22:31:05 +0300

    fixed my tailwind error

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="">
       {/* gonna make this actually a footer */}
       {/* need to install tailwind first */}
       Made by{" "}
-      <a href="https://github.com/yigit-cankurtaran" target="_blank">
+      <a
+        href="https://github.com/yigit-cankurtaran"
+        target="_blank"
+        className="text-red-500"
+      >
         Yiğit Cankurtaran
       </a>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js, ts, jsx, tsx, mdx}",
-    "./pages/**/*.{js, ts, jsx, tsx, mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     // hail mary lol
