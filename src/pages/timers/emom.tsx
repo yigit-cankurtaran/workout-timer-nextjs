18e734df6ff2aa2450997f4440e9fdbdd935e127
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-28 21:18:34 +0300

    skeleton styling for emom

--- src/pages/timers/emom.tsx
+++ src/pages/timers/emom.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function emom() {
-  return <div>emom</div>;
+  return (
+    <div className="flex bg-slate-900 text-gray-100 flex-col flex-grow h-screen justify-center items-center">
+      emom
+    </div>
+  );
 }
 
 export default emom;
