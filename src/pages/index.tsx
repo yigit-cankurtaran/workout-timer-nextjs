2c364f96315c4473a4742dd403b97a264698e6fd
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-28 21:11:16 +0300

    index styling + removal of input arrows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,26 @@ export default function Home() {
     <div className="flex-grow flex justify-center items-center flex-col w-full bg-slate-900 text-gray-100">
       <Link
         href="/timers/tabata"
-        className="text-red-400 hover:text-red-600 p-2 m-2"
+        className="text-red-400 hover:text-red-600 p-4 text-2xl font-bold m-4"
       >
         tabata
         {/* these can be bigger for mobile users and such */}
       </Link>
       <Link
         href="/timers/amrap"
-        className="text-red-400 hover:text-red-600 p-2 m-2"
+        className=" text-red-400 hover:text-red-600 p-4 text-2xl font-bold m-4"
       >
         amrap
       </Link>
       <Link
         href="/timers/emom"
-        className="text-red-400 hover:text-red-600 p-2 m-2"
+        className="text-red-400 hover:text-red-600 p-4 text-2xl font-bold m-4"
       >
         emom
       </Link>
       <Link
         href="/timers/hiit"
-        className="text-red-400 hover:text-red-600 p-2 m-2"
+        className="text-red-400 hover:text-red-600 p-4 text-2xl font-bold m-4"
       >
         hiit
       </Link>
