c5fc387dc80981cd2c79c856c4750028b7bfffb0
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-28 21:36:24 +0300

    styled hiit

--- src/pages/timers/hiit.tsx
+++ src/pages/timers/hiit.tsx
@@ -46,43 +46,50 @@ function tabata() {
   // implement proper rounds
 
   return (
-    <div>
+    <div className="flex flex-col justify-center min-h-screen bg-slate-900 text-gray-100">
       {/* if values aren't set bring up the setting part */}
       {!valuesSet && (
-        <div>
-          <label>minutes</label>
-          <input
-            type="number"
-            value={minutesInput}
-            onChange={(e) => setMinutesInput(e.target.value)}
-            // onChange might be a problem
-            // onKeyDown={(e) => {
-            //   if (e.key === "Enter") {
-            //     valueSetting();
-            //   }
-            // }}
-          />
-          <label>rounds</label>
-          <input
-            type="number"
-            value={rounds}
-            // if either this or the minutes have a problem change one
-            onChange={(e) => setRounds(parseInt(e.target.value))}
-          />
-          <label>length</label>
-          <input
-            type="number"
-            value={seconds}
-            onChange={(e) => setSeconds(parseInt(e.target.value))}
-          />
-          <label>rest length</label>
-          <input
-            type="number"
-            value={restSeconds}
-            onChange={(e) => setRestSeconds(parseInt(e.target.value))}
-          />
+        <div className="flex flex-col">
+          <div className="flex p-4 flex-col items-center ">
+            <p className="font-bold p-2">minutes</p>
+            <input
+              type="number"
+              value={minutesInput}
+              onChange={(e) => setMinutesInput(e.target.value)}
+              // onChange might be a problem
+              // onKeyDown={(e) => {
+              //   if (e.key === "Enter") {
+              //     valueSetting();
+              //   }
+              // }}
+              className="text-center bg-slate-900 text-gray-100 border-4 border-gray-100 p-2 rounded-lg"
+            />
+            <p className="font-bold p-2">rounds</p>
+            <input
+              type="number"
+              value={rounds}
+              // if either this or the minutes have a problem change one
+              onChange={(e) => setRounds(parseInt(e.target.value))}
+              className="text-center bg-slate-900 text-gray-100 border-4 border-gray-100 p-2 rounded-lg"
+            />
+            <p className="font-bold p-2">length</p>
+            <input
+              type="number"
+              value={seconds}
+              onChange={(e) => setSeconds(parseInt(e.target.value))}
+              className="text-center bg-slate-900 text-gray-100 border-4 border-gray-100 p-2 rounded-lg"
+            />
+            <p className="font-bold p-2">rest length</p>
+            <input
+              type="number"
+              value={restSeconds}
+              onChange={(e) => setRestSeconds(parseInt(e.target.value))}
+              className="text-center bg-slate-900 text-gray-100 border-4 border-gray-100 p-2 rounded-lg"
+            />
+          </div>
 
           <button
+            className="text-red-400 hover:text-red-600 p-2 m-2 bg-gray-800 rounded-lg w-32 h-10 self-center"
             onClick={() => {
               setValuesSet(true);
               console.log("minutes: " + minutesInput);
@@ -96,58 +103,66 @@ function tabata() {
         </div>
       )}
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         {workoutStarted && (
-          <div>
+          <div className="text-center">
             <h1>{workRunning ? "work" : "rest"}</h1>
             <p>rounds: {rounds}</p>
           </div>
         )}
+        {/* displays work or rest, whichever the user is doing */}
+        {workRunning && rounds > 0 && (
+          // it doesn't stop when rounds is 0, check and fix
+          // checking for it above and ending the workout might help
+          <div>
+            <CountdownCircleTimer
+              isPlaying
+              duration={seconds}
+              colors={["#004777", "#F7B801", "#A30000"]}
+              colorsTime={[seconds, 0]}
+              onComplete={() => {
+                console.log("timer ended by itself");
+                setWorkRunning(false);
+                setRestRunning(true);
+                setRounds(rounds - 1);
+              }}
+            >
+              {RenderTime}
+            </CountdownCircleTimer>
+          </div>
+        )}
+        {restRunning && (
+          <div>
+            <CountdownCircleTimer
+              isPlaying
+              duration={restSeconds}
+              colors={["#92C9E8", "#FFF4CC", "#F2A9A9"]}
+              colorsTime={[restSeconds, 0]}
+              onComplete={() => {
+                console.log("rest timer ended by itself");
+                setRestRunning(false);
+                setWorkRunning(true);
+              }}
+            >
+              {RenderTime}
+            </CountdownCircleTimer>
+          </div>
+        )}
       </div>
-      {/* displays work or rest, whichever the user is doing */}
-      {workRunning && rounds > 0 && (
-        // it doesn't stop when rounds is 0, check and fix
-        // checking for it above and ending the workout might help
-        <div>
-          <CountdownCircleTimer
-            isPlaying
-            duration={seconds}
-            colors={["#004777", "#F7B801", "#A30000"]}
-            colorsTime={[seconds, 0]}
-            onComplete={() => {
-              console.log("timer ended by itself");
-              setWorkRunning(false);
-              setRestRunning(true);
-              setRounds(rounds - 1);
-            }}
-          >
-            {RenderTime}
-          </CountdownCircleTimer>
-        </div>
-      )}
-      {restRunning && (
-        <div>
-          <CountdownCircleTimer
-            isPlaying
-            duration={restSeconds}
-            colors={["#92C9E8", "#FFF4CC", "#F2A9A9"]}
-            colorsTime={[restSeconds, 0]}
-            onComplete={() => {
-              console.log("rest timer ended by itself");
-              setRestRunning(false);
-              setWorkRunning(true);
-            }}
-          >
-            {RenderTime}
-          </CountdownCircleTimer>
-        </div>
-      )}
       {valuesSet && (
-        <div>
-          <button onClick={workoutStarted ? stopWorkout : startWorkout}>
+        <div className="flex flex-col justify-center items-center">
+          <button
+            className="text-red-400 hover:text-red-600 p-2 m-2 bg-gray-800 rounded-lg w-32 h-10 self-center"
+            onClick={workoutStarted ? stopWorkout : startWorkout}
+          >
             {workoutStarted ? "stop" : "start"}
           </button>
-          <button onClick={() => setValuesSet(false)}>edit values</button>
+          <button
+            className="text-red-400 hover:text-red-600 p-2 m-2 bg-gray-800 rounded-lg w-32 h-10 self-center"
+            onClick={() => setValuesSet(false)}
+          >
+            edit values
+          </button>
         </div>
       )}
     </div>
