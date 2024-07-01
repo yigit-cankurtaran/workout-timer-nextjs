50c95ab149b451e109d1087c4648c4b8bec9af6b
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-01 23:57:35 +0300

    started writing emom, wanna put start and stop to its own file

--- src/pages/timers/emom.tsx
+++ src/pages/timers/emom.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import WorkTimer from "@/components/worktimer";
+import RestTimer from "@/components/resttimer";
+import valueSetting from "@/hooks/valueSetting";
 
 // thinking about implementing this as counting down from a set time
 // input fields: minutes, work seconds, rest seconds
@@ -7,6 +10,12 @@ import React from "react";
 // work timer and rest timer for work seconds and rest seconds
 
 function emom() {
+  const [valuesSet, setValuesSet] = useState(false);
+  const [minutesInput, setMinutesInput] = useState("10");
+  const intMins = parseInt(minutesInput);
+  const [workSeconds, setWorkSeconds] = useState(40);
+  const [restSeconds, setRestSeconds] = useState(20);
+
   return (
     <div className="flex bg-slate-900 text-gray-100 flex-col flex-grow h-screen justify-center items-center">
       emom
