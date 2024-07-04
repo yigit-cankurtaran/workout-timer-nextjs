62b35bec23c5ff88bda5c8aa096711a68548584b
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-04 14:17:19 +0300

    some styling for footer and header

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,6 +12,7 @@ export default function Footer() {
           >
             Yiğit Cankurtaran
           </a>{" "}
+          with ❤️
         </p>
       </div>
     </footer>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <div className="pl-2 fixed top-0 bg-slate-900">
-      <Link href="/" className="text-2xl text-red-400 hover:text-red-600 ">
-        ← Home
+    <div className="m-2 p-2 fixed top-2 rounded-lg bg-slate-800">
+      <Link
+        href="/"
+        className="text-center text-2xl text-red-400 hover:text-red-600 "
+      >
+        ⬅️ Home
       </Link>
     </div>
   );
