90378cfd874b3fb9d8122421c62004f20bc66b70
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-08 20:36:08 +0300

    restoring my next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/workout-timer-nextjs",
   output: "export",
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
-  // attempting to fix the @ error
-  typescript: {
-    ignoreBuildErrors: true,
-    // !! WARN !! this is a workaround to fix the @ error
-    // i'll try to find a better solution
-    // for now just testing
-  },
 };
 
 module.exports = nextConfig;
