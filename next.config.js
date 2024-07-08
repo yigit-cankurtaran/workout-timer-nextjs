c29533e684dd0272045a8e13ad2b6551e97d60f0
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-08 19:29:29 +0300

    trying to build for github again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/workout-timer-next",
+  assetPrefix: "/workout-timer-next/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
