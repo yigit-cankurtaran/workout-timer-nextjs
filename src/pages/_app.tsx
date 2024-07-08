593ff4c7370e33e8d62a3782988f4d75d36d1137
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-08 20:02:24 +0300

    attempting to fix can't find component error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import { useRouter } from "next/router";
 import { HelmetProvider } from "react-helmet-async";
 
