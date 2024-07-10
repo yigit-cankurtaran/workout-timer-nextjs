3ad81fbfd297717705305a24d15759b2cb9a92df
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-10 14:00:44 +0300

    amrap and for time test

---
+++ src/tests/amrap.test.tsx
@@ -0,0 +1,113 @@
+// mocking helmet because of an error
+jest.mock("react-helmet-async", () => ({
+  Helmet: ({ children }: { children: React.ReactNode }) => children,
+  HelmetProvider: ({ children }: { children: React.ReactNode }) => children,
+}));
+
+// mocking toast to fix the "2 set" issue due to the toast message
+jest.mock("@/stuff/CustomToast", () => ({
+  SuccessToast: jest.fn(() => null),
+  ErrorToast: jest.fn(() => null),
+}));
+
+// mocking the window functionality for the matchMedia
+beforeAll(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false, // You can customize this to match specific queries for your tests
+      media: query,
+      onchange: null,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import Amrap from "@/pages/timers/amrap";
+// next 2 fix the toBeInTheDocument issue
+import "@testing-library/jest-dom/jest-globals";
+import "@testing-library/jest-dom";
+
+describe("Tabata Timer", () => {
+  test("set button sets the proper value", async () => {
+    const { getByText } = render(<Amrap />);
+    // using a function to provide more flexible matcher
+    const setButton = getByText(/set/i);
+    // regex, i means case insensitive
+    fireEvent.click(setButton);
+
+    // text that only appears when the values are set
+    // waitFor to handle conditional rendering
+    await waitFor(() => {
+      expect(getByText(/start/i)).toBeInTheDocument();
+      expect(getByText(/edit/i)).toBeInTheDocument();
+    });
+  });
+  test("start button starts the timer", async () => {
+    const { getByText, findByText } = render(<Amrap />);
+
+    const setButton = getByText(/set/i);
+    fireEvent.click(setButton);
+
+    // waiting for the start button to appear
+    const startButton = await findByText(/start/i);
+    fireEvent.click(startButton);
+
+    // checking for the new buttons that appear after the timer starts
+    // these are the buttons that appear after the timer starts
+    await findByText(/stop/i);
+    await findByText(/edit/i);
+  });
+  test("stop button stops the timer", async () => {
+    const { getByText, findByText } = render(<Amrap />);
+
+    const setButton = getByText(/set/i);
+    fireEvent.click(setButton);
+
+    const startButton = await findByText(/start/i);
+    fireEvent.click(startButton);
+
+    const stopButton = await findByText(/stop/i);
+    fireEvent.click(stopButton);
+
+    // checking for the new buttons that appear after the timer stops
+    await findByText(/start/i);
+    await findByText(/edit/i);
+  });
+  test("edit button allows to edit the timer", async () => {
+    const { getByText, findByText } = render(<Amrap />);
+
+    const setButton = getByText(/set/i);
+    fireEvent.click(setButton);
+
+    const startButton = await findByText(/start/i);
+    fireEvent.click(startButton);
+
+    const editButton = await findByText(/edit/i);
+    fireEvent.click(editButton);
+
+    // checking for the new buttons that appear after the timer stops
+    await findByText(/set/i);
+  });
+  test("invalid value setting stays in the same screen", async () => {
+    const { getByText, findByText } = render(<Amrap />);
+
+    // get the input
+    const input = getByText(/minutes/i).nextElementSibling as HTMLInputElement;
+
+    // set the input value to an invalid value
+    fireEvent.change(input, { target: { value: "0" } });
+
+    // set the value
+    const setButton = getByText(/set/i);
+    fireEvent.click(setButton);
+
+    // check that it stays on the same page
+    await findByText(/minutes/i);
+    await findByText(/set/i);
+  });
+});
