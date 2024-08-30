caee2fab05fec65e51fd8a312d707297f2619059
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-08-30 15:17:18 +0300

    removed unused import

--- src/pages/timers/amrap.tsx
+++ src/pages/timers/amrap.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Helmet } from "react-helmet-async";
 import minutesToSeconds from "@/helpers/minutesToSeconds";
 import valueSetting from "@/helpers/valueSetting";
 import WorkoutDisplay from "@/stuff/WorkoutDisplay";
